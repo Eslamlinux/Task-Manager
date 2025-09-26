fa2a90fa7fa5b2b4d7b16fabd6eee9425e5997de
Eslam Linux <linuxeslam@gmail.com>
2025-09-26 04:33:30 +0300

    Update register_dialog.cpp

--- register_dialog.cpp
+++ register_dialog.cpp
@@ -19,3 +19,30 @@ RegisterDialog::RegisterDialog(wxWindow* parent, UserManager* userManager)
     titleFont.SetWeight(wxFONTWEIGHT_BOLD);
     titleText->SetFont(titleFont);
     
+    mainSizer->Add(titleText, 0, wxALL | wxALIGN_CENTER, 15);
+    
+    // Form
+    wxFlexGridSizer* formSizer = new wxFlexGridSizer(5, 2, 10, 10);
+    formSizer->AddGrowableCol(1);
+    
+    // Username
+    formSizer->Add(new wxStaticText(this, wxID_ANY, "Username:"), 0, wxALIGN_CENTER_VERTICAL);
+    usernameCtrl = new wxTextCtrl(this, wxID_ANY);
+    formSizer->Add(usernameCtrl, 0, wxEXPAND);
+    
+    // Full Name
+    formSizer->Add(new wxStaticText(this, wxID_ANY, "Full Name:"), 0, wxALIGN_CENTER_VERTICAL);
+    fullNameCtrl = new wxTextCtrl(this, wxID_ANY);
+    formSizer->Add(fullNameCtrl, 0, wxEXPAND);
+    
+    // Email
+    formSizer->Add(new wxStaticText(this, wxID_ANY, "Email:"), 0, wxALIGN_CENTER_VERTICAL);
+    emailCtrl = new wxTextCtrl(this, wxID_ANY);
+    formSizer->Add(emailCtrl, 0, wxEXPAND);
+    
+    // Password
+    formSizer->Add(new wxStaticText(this, wxID_ANY, "Password:"), 0, wxALIGN_CENTER_VERTICAL);
+    passwordCtrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, 
+                                 wxDefaultSize, wxTE_PASSWORD);
+    formSizer->Add(passwordCtrl, 0, wxEXPAND);
+    
