b38a4064edcb28da10fea16ba63b2fc3462ed006
Eslam Linux <linuxeslam@gmail.com>
2025-09-26 04:48:42 +0300

    Create category_dialog.cpp

---
+++ category_dialog.cpp
@@ -0,0 +1,30 @@
+#include "category_dialog.h"
+
+wxBEGIN_EVENT_TABLE(CategoryDialog, wxDialog)
+    EVT_BUTTON(wxID_SAVE, CategoryDialog::OnSaveButton)
+    EVT_BUTTON(wxID_CANCEL, CategoryDialog::OnCancelButton)
+wxEND_EVENT_TABLE()
+
+CategoryDialog::CategoryDialog(wxWindow* parent, CategoryManager* categoryManager, 
+                             int userId, Category* category)
+    : wxDialog(parent, wxID_ANY, (category ? "Edit Category" : "Add Category"), 
+              wxDefaultPosition, wxSize(400, 300),
+              wxDEFAULT_DIALOG_STYLE | wxCENTER),
+      categoryManager(categoryManager), userId(userId), 
+      categoryId(category ? category->id : -1) {
+    
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    
+    // Form
+    wxFlexGridSizer* formSizer = new wxFlexGridSizer(3, 2, 10, 10);
+    formSizer->AddGrowableCol(1);
+    
+    // Name
+    formSizer->Add(new wxStaticText(this, wxID_ANY, "Name:"), 0, wxALIGN_CENTER_VERTICAL);
+    nameCtrl = new wxTextCtrl(this, wxID_ANY);
+    formSizer->Add(nameCtrl, 0, wxEXPAND);
+    
+    // Color
+    formSizer->Add(new wxStaticText(this, wxID_ANY, "Color:"), 0, wxALIGN_CENTER_VERTICAL);
+    colorCtrl = new wxColourPickerCtrl(this, wxID_ANY);
+    formSizer->Add(colorCtrl, 0, wxEXPAND);
